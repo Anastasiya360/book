97d0440412db6f3d08b8495e3daa53588e9fe32d
Anastasiya <n.bogocharova@gmail.com>
2024-09-24 12:58:33 +0300

    first commit

---
+++ src/main/java/com/example/book/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.example.book.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book",
+                description = "Interaction with books", version = "2.6.0",
+                contact = @Contact(
+                        name = "Anastasia",
+                        email = "n.bogocharova@gmail.com"
+                )
+        )
+)
+public class OpenApiConfig {
+
+}
+
+
