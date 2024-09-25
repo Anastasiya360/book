ef08da014f074173974115f8d8d3bb49148dd893
Anastasiya <n.bogocharova@gmail.com>
2024-09-25 16:17:56 +0300

    added @Schema

--- src/main/java/com/example/book/model/Book.java
+++ src/main/java/com/example/book/model/Book.java
@@ -1,8 +1,6 @@
 package com.example.book.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,15 +12,18 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class Book implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Уникальный идентификатор записи", requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer id;
 
+    @Schema(description = "Наименование книги", requiredMode = Schema.RequiredMode.REQUIRED)
     private String title;
 
+    @Schema(description = "Автор книги", requiredMode = Schema.RequiredMode.REQUIRED)
     private String author;
 
+    @Schema(description = "Описание")
     private String description;
 
+    @Schema(description = "Цена книги", requiredMode = Schema.RequiredMode.REQUIRED)
     private Double price;
 }
